5940e347a55a915098d4b6acdf5f91eda9ea0349
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-05-01 22:25:15 +0300

    minor

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -64,6 +64,7 @@ const RightBar = () =>{
                 .from("profiles")
                 .select("*")
                 .not("id", "in", `(${excludeIds.join(",")})`)
+                .limit(5)
             if (error) throw new Error(error.message);
             return data;
         },
@@ -101,90 +102,6 @@ const RightBar = () =>{
                 <PopBook book={book} key={book.id} />
             ))}
           </div>
-          {/* <div className="item">
-            <span>Latest Activities </span>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
-                  alt=""
-                />
-                <p>
-                  <span>Jane Doe</span> Changed their status
-                </p>
-              </div>
-              <span>5 min ago</span>
-            </div>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
-                  alt=""
-                />
-                <p>
-                  <span>Jane Doe</span> Changed their profile decoration
-                </p>
-              </div>
-              <span>6 min ago</span>
-            </div>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
-                  alt=""
-                />
-                <p>
-                  <span>Jane Doe</span> Changed their work status
-                </p>
-              </div>
-              <span>7 min ago</span>
-            </div>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
-                  alt=""
-                />
-                <p>
-                  <span>Jane Doe</span> Changed their relationship status
-                </p>
-              </div>
-              <span>8 min ago</span>
-            </div>
-          </div> */}
-          {/* <div className="item">
-            <span>Online Friends</span>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
-                  alt=""
-                />
-                <div className="online" />
-                <span>Jane Doe</span>
-              </div>
-            </div>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
-                  alt=""
-                />
-                <div className="online" />
-                <span>Jane Doe</span>
-              </div>
-            </div>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
-                  alt=""
-                />
-                <div className="online" />
-                <span>Jane Doe</span>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     );
